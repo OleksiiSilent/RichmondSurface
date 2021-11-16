b9fcc3cdeefd032cf84e31fff06ccf024b1bf57a
Alexey Narbutovich <anarbutovich@gmail.com>
2021-11-16 16:44:30 +0200

    add webpack and its plugins

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: "production",
+    entry: {
+        main: path.resolve(__dirname, './src/js/main.js')
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: "[name].bundle.js"
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Extended Reality',
+            template: path.resolve(__dirname, './src/template.html'),
+            filename: 'index.html'
+            }),
+        new CleanWebpackPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }
+        ]
+    }
+}
